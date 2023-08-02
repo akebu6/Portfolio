7959eddacf2fad71f439ecf0806a97b392f88d80
akebu6 <asimasiku999@gmail.com>
2023-08-02 21:23:25 +0200

    chore: removed title

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.title('Hello World')
-
 selected = option_menu(
   menu_title=None,
   options=['Home', 'About', 'Projects', "Resume"],
-  icons=['🏠', '📝', '📂', '📄'],
+  icons=['house-door', 'patch-question', 'kanban', 'file-earmark-person'],
   default_index=0,
   orientation='horizontal',
 )
 
 # The Home page view
+# The home page will have some basic information about me and my interests as well as well include a picture of me, my resume, and a link to my socials: GitHub LinkedIn, Twitter and Kaggle.
 
 # The About page view
 
